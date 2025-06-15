887eaad73c667f6681501d1e3d614380a9111176
Oleg <displacer88@gmail.com>
2025-06-15 13:36:44 +0300

    fix: исправил замечания ревьюера

--- src/components/ProtectedRouteElement/ProtectedRouteElement.tsx
+++ src/components/ProtectedRouteElement/ProtectedRouteElement.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAppSelector } from '@shared/services/hooks';
+import { Spinner } from '@components/Spinner';
 
 export const ProtectedRouteElement: FC<{ element: React.ReactNode }> = ({
   element,
 }) => {
-  const userData = useAppSelector((state) => state.user);
+  const { isAuth, initLoading } = useAppSelector((state) => state.user);
   const location = useLocation();
   const from = location.state?.from || location.pathname || '/';
 
-  if (!userData.isAuth) {
+  if (initLoading) {
+    return <Spinner description='Загрузка профиля пользователя...' />; // Здесь можете использовать ваш компонент прелоадера
+  }
+
+  if (!isAuth) {
     return <Navigate to='/login' state={{ from }} />;
   }
 
